afa939912f580e9f42be41da6b055da376919472
Orlando <baello2020@gmail.com>
2021-03-07 18:43:00 -0800

    Updates

---
+++ precache-manifest.fd90045b27e7f3a93e558ffa85239821.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c507899e1f9125ffbdd5dce9115f50c",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "9a2f76acbfa2a1242b71",
+    "url": "/React_Portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3275287a8a515dc12a76",
+    "url": "/React_Portfolio/static/css/main.57f5244b.chunk.css"
+  },
+  {
+    "revision": "9a2f76acbfa2a1242b71",
+    "url": "/React_Portfolio/static/js/2.5902d740.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React_Portfolio/static/js/2.5902d740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3275287a8a515dc12a76",
+    "url": "/React_Portfolio/static/js/main.d0ba38f3.chunk.js"
+  },
+  {
+    "revision": "861a559bea7a897ebcc6",
+    "url": "/React_Portfolio/static/js/runtime-main.143b31fe.js"
+  },
+  {
+    "revision": "2fd4bf47d9dcaf7c3d0fe0c2a9016861",
+    "url": "/React_Portfolio/static/media/HomeProfile.2fd4bf47.jpg"
+  },
+  {
+    "revision": "dc89473ec2677ccc9fe6a9a6d38bd8e9",
+    "url": "/React_Portfolio/static/media/Profile.dc89473e.jpeg"
+  },
+  {
+    "revision": "f45dddb15491196559acc6bd910b25b3",
+    "url": "/React_Portfolio/static/media/contact.f45dddb1.png"
+  },
+  {
+    "revision": "053853574eb5671f5ba690588da2626a",
+    "url": "/React_Portfolio/static/media/pano-dam.05385357.jpg"
+  }
+]);
